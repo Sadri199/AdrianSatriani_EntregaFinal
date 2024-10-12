1f2b6e35403296231410741e1a216567e4319fd3
Sadri199 <adrsa_15@hotmail.com>
2024-10-12 19:36:24 -0300

    Last minute fixes #1

--- tercera_entrega/movies/forms.py
+++ tercera_entrega/movies/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 class AddingMovie (forms.Form): #I wanted to make a dropdown with a Genre value but it was too hard and i prefer to leave it for the next project.
     
-    name = forms.CharField(max_length=50)
-    plot = forms.CharField(max_length=50, required=False)
-    main_actor = forms.CharField(max_length=20)
-    year = forms.IntegerField(min_value=1950)
+    name = forms.CharField(max_length=50, min_length=5)
+    plot = forms.CharField(max_length=100, min_length=5, required=False)
+    main_actor = forms.CharField(max_length=20, min_length=4)
+    year = forms.IntegerField(min_value=1950, max_value=2030)
     
 class SearchMovie (forms.Form):
     
--- tercera_entrega/movies/models.py
+++ tercera_entrega/movies/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class MoviesDB (models.Model): #I wanted to make a dropdown with a Genre value but it was too hard and i prefer to leave it for the next project.
     name = models.CharField(max_length=50)
-    plot = models.CharField(max_length=50)
+    plot = models.CharField(max_length=100)
     main_actor = models.CharField(max_length=20)
     year = models.IntegerField()
     
